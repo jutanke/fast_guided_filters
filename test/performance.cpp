5f4b22e89fad9c7258ec31733540a4e4ba27f180
root <jutanke@googlemail.com>
2017-07-12 21:24:33 +0000

    improved sample code

--- test/performance.cpp
+++ test/performance.cpp
@@ -10,15 +10,19 @@ int main()
 
 	cv::Mat blur;
 	size_t r = 4;
-	double eps = 3.4;
+	double eps = 0.2 * 0.2;
 	size_t s = 4;
 	fgf::blur(image, blur, r, eps, s);
 
 	cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
-  cv::imshow("Display Image", blur);
+  cv::imshow("Display Image", image);
+	cv::waitKey(0);
 
 
-  cv::waitKey(0);
+	cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
+	cv::imshow("Display Image", blur);
+	cv::waitKey(0);
+
 
 	return 0;
 }
